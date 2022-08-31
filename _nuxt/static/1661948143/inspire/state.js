47b173a6f9d07b9cb66aed2e22e3b30f4b0902f4
cleyton2charm <tom.soarez@gmail.com>
2022-08-31 14:33:55 -0300

    727fedd

---
+++ _nuxt/static/1661948143/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"vizinho\u002F_nuxt\u002Fstatic\u002F1661948143",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"vizinho\u002F",assetsPath:"vizinho\u002F_nuxt\u002F",cdnURL:a}}}}(null));
